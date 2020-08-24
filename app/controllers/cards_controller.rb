b92e1a9a73ef29b381ae2a2ec7f1d12d106e63dd
soncrayon <arnold.curtis@gmail.com>
2020-08-23 22:27:23 -0400

    update controllers and serializers for new card attribute names

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,9 +1,7 @@
 class CardsController < ApplicationController
 
-    before_action :set_reading, :set_user, only: [:create, :show, :destroy]
+    before_action :set_reading, only: [:create, :show, :destroy]
     before_action :set_card, only: [:show, :destroy]
-    # don't know if the before_action helper methods need to be set out separately 
-    # before_action :set_reading
     
     def index 
         render json: Card.all
@@ -29,12 +27,9 @@ class CardsController < ApplicationController
 
     def card_params
         
-        params.require(:card).permit(:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id)
-
-        # make sure to update the frontend to reflect the newly-named card attributes 
-        # params.require(:card).permit(:past => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id],
-        # :present => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id],
-        # :future => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id])
+        params.require(:card).permit(:past => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id],
+        :present => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id],
+        :future => [:id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id])
         
     end
 
@@ -46,11 +41,6 @@ class CardsController < ApplicationController
         # the below is not giving me what I need in terms of format (maybe just get raw timestamp then handle on frontend?)
         # now = Time.new.strftime("%a, %B %d, %Y at %I:%M %p")
         @reading = Reading.create
-    end
-
-    def set_user
-        # temporary for seed data
-        @user = User.first
     end
 
 end
--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,5 +1,5 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation
+  attributes :id, :period, :name, :full_meaning, :upright_meaning, :reversed_meaning, :image, :orientation, :reading_id, :user_id 
   has_one :reading, optional: true 
   has_one :user, optional: true
 end
