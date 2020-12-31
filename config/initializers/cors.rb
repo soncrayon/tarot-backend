b14de6f1621a90b164bf4f091d3e295100180dc7
soncrayon <arnold.curtis@gmail.com>
2020-12-31 00:35:44 -0500

    update initializers --> cors.rb to update origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://soncrayon.github.io/tarot-frontend/'
+    origins 'https://soncrayon.github.io'
 
     resource '*',
       headers: :any,
